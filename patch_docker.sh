545a94c2fef52796d5cef8dcdb075400ac837ce1
cestpasphoto <cestpasphoto@gmail.com>
2021-10-20 11:06:14 +0200

    Add script to patch a docker

---
+++ patch_docker.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+### Edit these variables
+DOCKER_CONTAINER="firefly_fireflyiii_1"
+DOCKER_COMPOSE="/var/www/firefly/docker-compose.yml"
+
+
+### Apply patch(es)
+apply_patch () {
+  # Local patch or local patches or remote patch file ?
+  if [[ -f "frontend.patch" ]]; then
+    cat frontend.patch resources.patch app.patch > global.patch
+    if [[ -f "public.patch" ]]; then
+      cat public.patch >> global.patch
+    fi
+  elif [[ -f "global.patch" ]]; then
+    :
+  else
+    wget "https://github.com/cestpasphoto/firefly-iii/raw/main/global.patch"
+  fi
+
+  sudo docker cp global.patch ${DOCKER_CONTAINER}:/var/www/html/
+  sudo docker exec -it ${DOCKER_CONTAINER} /bin/bash -c '
+    patch -s -p0 < global.patch
+    rm global.patch
+  '
+}
+
+### Clean docker from previous patches (redownload image)
+clean_docker () {
+  sudo docker container stop ${DOCKER_CONTAINER}
+  sudo docker container rm ${DOCKER_CONTAINER}
+  sudo docker-compose -f ${DOCKER_COMPOSE} up -d
+}
+
+### Prepare environment to compile and generate patches
+install_env () {
+  sudo docker exec -it ${DOCKER_CONTAINER} /bin/bash -c '
+    # backup stuff
+    cp -a public    public.backup
+    cp -a frontend  frontend.backup
+    cp -a resources resources.backup
+    cp -a app       app.backup
+
+    # install nodejs and npm, and update to latest version, install laravel mix, and compile
+    apt-get update ; apt-get install -y npm ; npm cache clean -f ; npm install -g n ; n lts ; PATH="$PATH"
+    cd frontend ; npm install ; cd /var/www/html/
+  '
+}
+
+# Compile and generate patches
+build () {
+  sudo docker exec -it ${DOCKER_CONTAINER} /bin/bash -c '
+    # Compile
+    cd frontend ; npm run prod ; cd /var/www/html/
+
+    # Create patches
+    diff -Naur public.backup/       public/       > public.patch
+    diff -Naur frontend.backup/src/ frontend/src/ > frontend.patch
+    diff -Naur resources.backup/    resources/    > resources.patch
+    diff -Naur app.backup/          app/          > app.patch
+    cat public.patch frontend.patch resources.patch app.patch > global.patch
+    rm  public.patch frontend.patch resources.patch app.patch
+  '
+
+  # Get the patch on host
+  sudo docker cp ${DOCKER_CONTAINER}:/var/www/html/global.patch .
+}
+
+###############################################################################
+
+#clean_docker
+#install_env
+#apply_patch
+#build
+
+###clean_docker
+###apply_patch
--- readme.md
+++ readme.md
@@ -6,13 +6,18 @@ Based on the great repo https://github.com/firefly-iii/firefly-iii with the foll
 
 * [x] Updated UI
   * [x] Dashboard top bar: added shortcut to new expense and new transfer, merge settings and user menu
+  * [x] Simplified dashboard: removed several boxes that I didn't used and that couldn't be disabled in settings
+  * [x] Time period of 1 week and 1 month are now sliding periods, not fixed ones. And this period is updated every time.
 * [x] Account reconciliation
+  * [ ] Automatic: just input your latest balance and it'll find which operations to select
+  * [x] Added shortcut on dashboard to reconcilate any account
   * [x] Forced to be over last month instead of current settings
   * [x] Start without any button press
-* [x] Recurring transactions
-  * [x] Create them earlier (1 week before) and run the uncreated past ones (if you forget to run cron in the last 3 days)
+* [x] Transactions
+  * [x] Creation form: reorganised and simplified
+  * [x] Recurring: create them earlier (1 week before) and run the uncreated past ones (if you forget to run cron in the last 3 days)
 
 ### What if I want such features in my docker
 
-Edit configuration in `./patch_docker.sh`, then run it where your .yml is
-This file runs the toolchain if any updates on .js files.
+Download `patch_docker.sh`, edit config in first lines, then run it.
+No need to clone this repo. Also this script runs the toolchain if any updates on .js files.
